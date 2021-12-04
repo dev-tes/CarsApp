47cfc2ea24ea4ac9e3a21c83a5bd18cb8fd7f884
dev-tes <tesleem.amuda@gmail.com>
2021-12-04 06:52:02 +0100

    add viewControllers to tab bar

--- CarsApp/CarsApp/TabBarViewController.swift
+++ CarsApp/CarsApp/TabBarViewController.swift
@@ -7,11 +7,27 @@
 
 import UIKit
 
-class TabBarViewController: UIViewController {
+class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // MARK: Create instance of viewControllers
+              navigationController?.isNavigationBarHidden = true
+                let homeViewController = ExploreViewController()
+                let favoriteViewController = FavouriteViewController()
+                let notificationViewController = NotificationViewController()
+                let chatViewController = ChatViewController()
+        // MARK: Assign viewController to tab Bar
+              self.setViewControllers([homeViewController, favoriteViewController, notificationViewController, chatViewController], animated: false)
+              guard let items = self.tabBar.items else { return }
+              let images = ["house.fill", "magnifyingglass", "bell.fill", "message.fill"]
+              for item in 0...3 {
+                items[item].image = UIImage(systemName: images[item])
+              }
+        // MARK: Changing tint Color
+        self.tabBar.tintColor = .systemGray
+    
     }
     
 
--- CarsApp/CarsApp/ViewController.swift
+++ CarsApp/CarsApp/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ExploreViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
