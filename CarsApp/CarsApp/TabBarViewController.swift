97e0005fca33a74ee27b4ff5129367f714150e82
dev-tes <tesleem.amuda@gmail.com>
2021-12-04 06:44:23 +0100

    create tab bar view controller

---
+++ CarsApp/CarsApp/TabBarViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarViewController.swift
+//  CarsApp
+//
+//  Created by  Decagon on 04/12/2021.
+//
+
+import UIKit
+
+class TabBarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
